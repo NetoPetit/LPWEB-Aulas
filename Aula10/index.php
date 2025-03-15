425f45ed2cf124eede7aa14b84906e80abd7a4ae
NetoPetit <136471249+NetoPetit@users.noreply.github.com>
2025-03-14 22:27:41 -0300

    exercicio html aula get post

---
+++ Aula10/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 10</title>
+</head>
+<body>
+    <form action='php/recebe.php' method='GET'>
+        <input type='text' name='usuario'/>
+        <input type='password' name='senha'>
+        <input type='submit' value='Enviar'/>
+    </form>
+</body>
+</html>
---
+++ Aula10/php/recebe.php
@@ -0,0 +1,7 @@
+<?php
+
+    echo $_GET['usuario'];
+    echo "<br>";
+    echo $_GET['senha'];
+    
+?>
