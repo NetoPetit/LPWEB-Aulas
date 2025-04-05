3e2dae1ecff020f44f2b8be4d4c29c1f8a4b8c08
NetoPetit <136471249+NetoPetit@users.noreply.github.com>
2025-04-04 22:20:26 -0300

    atualização

--- Aula15/outros/aluno_novo_salvar.php
+++ Aula15/outros/aluno_novo_salvar.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -42,6 +42,6 @@
         header('location: aluno_consulta.php');
 
     ?>
-    <button onclick="javascript:history.go(-1)">Voltar</button>
+    
 </body>
 </html>
